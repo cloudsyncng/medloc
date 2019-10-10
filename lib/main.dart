15a6725050b258f6e459b45519501ba3c476a33e
ISMAILAWA <ismailawa.aliyu@gmail.com>
2019-10-10 15:15:46 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MedPlus());
+
+class MedPlus extends StatelessWidget {
+  const MedPlus({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(),
+    );
+  }
+}
